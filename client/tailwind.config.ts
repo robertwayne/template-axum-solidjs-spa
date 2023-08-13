0c1a272ada4853f263af6b5b7b35032142367ecd
Rob Wagner <rob@sombia.com>
2023-08-13 19:02:07 -0400

    Inline initial theme style to avoid FOUC

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,11 +4,13 @@ export default {
         extend: {
             colors: {
                 light: {
+                    // This color is also inlined in `index.html`.
                     primary: "#f8f9fa",
                     secondary: "#272727",
                     highlight: "#3f5ab1",
                 },
                 dark: {
+                    // This color is also inlined in `index.html`.
                     primary: "#222831",
                     secondary: "#dddbd8",
                     highlight: "#ffc857",
