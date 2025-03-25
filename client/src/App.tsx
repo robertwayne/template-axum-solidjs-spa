b321747c82b4c03aac11234eddf5864f66e692b6
Rob Wagner <rob@sombia.com>
2025-03-25 18:00:19 -0400

    Fix some minor CSS issues with page sizing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,10 @@ import NavHeader from "./components/NavHeader"
 
 export const App = (props: ParentProps) => {
     return (
-        <div class="flex w-full h-full max-w-screen-2xl flex-col bg-[var(--primary)] text-[var(--secondary)] text-secondary transition">
+        <div class="flex w-full h-full flex-col bg-[var(--primary)] text-[var(--secondary)] text-secondary transition max-w-[1200px] justify-center mx-auto">
             <NavHeader />
 
-            <main class="flex h-full w-full grow flex-col items-center p-4">
+            <main class="flex w-full grow flex-col items-center">
                 {props.children}
             </main>
 
