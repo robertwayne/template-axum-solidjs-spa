0d164a0a1d7083a8b94ccdd4e38ca11c3f2f9d21
Rob Wagner <rob@sombia.com>
2025-04-29 23:42:18 -0400

    fix clippy lints

--- server/src/main.rs
+++ server/src/main.rs
@@ -61,7 +61,7 @@ async fn main() -> Result<(), AppError> {
         let host = std::env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
         let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
-        format!("{}:{}", host, port).parse::<SocketAddr>()?
+        format!("{host}:{port}").parse::<SocketAddr>()?
     };
 
     let router = Router::new()
